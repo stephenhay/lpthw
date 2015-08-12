4faaef3e456993e1b64db7017d758c6b88b2bfcb
Stephen Hay <stephen@zerointerface.nl>
2015-08-12 11:52:15 +0200

    Exercise 1

---
+++ ex1_study_drill_2.py
@@ -0,0 +1,10 @@
+# Study drills 2 and 3 combined:
+# Printing only one line by commenting out the other lines.
+print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+# print "This is 'another line'."
