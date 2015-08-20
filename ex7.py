67e4fc24e3a99f217957a2ad63a369c4e0b58735
Stephen Hay <stephen@zerointerface.nl>
2015-08-20 21:19:38 +0200

    exercise 7

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# print the string
+print "Mary had a little lamb."
+# print the string, replacing %s with 'snow'
+print "It's fleece was white as %s." % 'snow'
+# print the string
+print "and everywhere that Mary went."
+# print the string '.' 10 times
+print "." * 10 # what'd that do?
+
+# assign a letter to each variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# print the first 6 variables, allow next print statement to be on same line
+print end1 + end2 + end3 + end4 + end5 + end6,
+# print the last 6 variables
+print end7 + end8 + end9 + end10 + end11 + end12
